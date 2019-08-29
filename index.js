15152e5cb8371fafba770f73dfda802b627f2abd
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-29 20:06:35 +0000

    put the function together

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fetchMyIP = require("./iss");
 const fetchCoordsByIP = require("./iss");
+const fetchISSFlyOverTimes = require("./iss")
+const nextISSTimesForMyLocation = require("./iss")
 
 
 // fetchMyIP((error, ip) => {
@@ -10,12 +12,34 @@ const fetchCoordsByIP = require("./iss");
 //   }
 // });
 
-fetchCoordsByIP("66.207.199.230", (err, desc) => {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(desc);
+// fetchCoordsByIP("66.207.199.230", (err, desc) => {
+//   if (err) {
+//     console.log(err)
+//   } else {
+//     console.log(desc);
+//   }
+// });
+
+// fetchISSFlyOverTimes({ latitude: '43.63830', longitude: '-79.43010' }, (err, desc) => {
+//   if (err) {
+//     console.log("error is:", err)
+//   } else {
+//     console.log(desc);
+//   }
+// });
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
   }
+  // success, print out the deets!
+  passTimes.forEach(pass => {
+    console.log(`Next Pass at ${(new Date(pass.risetime * 1000))} for ${pass.duration} seconds!`)
+  })
 });
+
+
+
+// { latitude: '43.63830', longitude: '-79.43010' }
 
 // 66.207.199.230
--- iss.js
+++ iss.js
@@ -7,20 +7,20 @@ const request = require("request");
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
-// const fetchMyIP = (callback) => {
-//   // console.log("callback is:", callback);
-//   request('https://api.ipify.org/?format=json', (err, resp, body) => {
-//     if (err) {
-//       console.log("error is:", error)
-//       callback(err, null);
-//     } else if (resp.statusCode !== 200) {
-//       callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null)
-//     } else {
-//       console.log("body is:", JSON.parse(body).ip)
-//       callback(null, JSON.parse(body).ip);
-//     }
-//   })
-// }
+const fetchMyIP = (callback) => {
+  // console.log("callback is:", callback);
+  request('https://api.ipify.org/?format=json', (err, resp, body) => {
+    if (err) {
+      // console.log("error is:", error)
+      callback(err, null);
+      // } else if (resp.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null)
+    } else {
+      // console.log("body is:", JSON.parse(body).ip)
+      callback(null, JSON.parse(body).ip);
+    }
+  })
+}
 
 const fetchCoordsByIP = (IP, callback) => {
   request(`https://ipvigilante.com/${IP}`, (err, resp, body) => {
@@ -28,9 +28,50 @@ const fetchCoordsByIP = (IP, callback) => {
       callback(err, null);
     } else if (resp.statusCode !== 200) {
       callback(Error(`Status Code ${resp.statusCode} when fetching IP. Response: ${body}`), null)
+    } else {
+      data = {
+        latitude: JSON.parse(body).data.latitude,
+        longitude: JSON.parse(body).data.longitude
+      }
+      callback(null, data);
     }
-    callback(null, body);
+  });
+}
+
+const fetchISSFlyOverTimes = (coords, callback) => {
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (err, resp, body) => {
+    if (err) {
+      callback(err, null);
+    } else if (resp.statusCode !== 200) {
+      callback(Error(`Status Code ${resp.statusCode} when fetching IP. Response: ${body}`), null)
+    } else {
+      callback(null, JSON.parse(body).response);
+    }
   })
 }
 
-module.exports = fetchCoordsByIP;
+const nextISSTimesForMyLocation = function (callback) {
+  fetchMyIP((error, ip) => {
+    if (error) {
+      console.log("Error is:", error);
+    } else {
+      fetchCoordsByIP(ip, (err, desc) => {
+        if (err) {
+          console.log(err)
+        } else {
+          fetchISSFlyOverTimes(desc, (err, desc) => {
+            if (err) {
+              console.log("error is:", err)
+            } else {
+              callback(null, desc);
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+module.exports = nextISSTimesForMyLocation;
+
+// { latitude: '43.63830', longitude: '-79.43010' }
